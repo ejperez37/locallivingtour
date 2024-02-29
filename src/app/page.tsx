3b246530143a7fad9ef70f863810bd0a642743fa
E.J. Perez <ejperez37@gmail.com>
2024-02-28 21:14:28 -0500

    coming soon image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <h1>coming soon</h1>
+  return (
+    <div className="flex justify-center items-center w-dvw h-dvh">
+      <Image
+        src="/comingsoonlogo.png"
+        alt="local living tour, coming soon!"
+        width={500}
+        height={500}
+      />
+    </div>
+  )
 }
